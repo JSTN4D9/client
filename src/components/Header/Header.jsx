78e41ae36d8249bb3fafcc2c57afe258c15c35a7
Justin Uminga <justinuminga@gmail.com>
2025-05-31 20:30:03 +0800

    modal windows

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,7 @@
-// src/components/Header/Header.jsx
 import TopBar from "../TopBar/TopBar";
 import Navbar from "../NavBar/NavBar";
-import { AppBar, Slide, useScrollTrigger, Button, Box } from "@mui/material";
+import { AppBar, Slide, useScrollTrigger } from "@mui/material";
 import PropTypes from "prop-types";
-import { useState } from "react";
-import SignInModal from "../AuthModals/SignInModal";
-import SignUpModal from "../AuthModals/SignUpModal";
 
 function HideOnScroll(props) {
   const { children, window } = props;
@@ -26,46 +22,14 @@ HideOnScroll.propTypes = {
 };
 
 export default function Header(props) {
-  const [openSignIn, setOpenSignIn] = useState(false);
-  const [openSignUp, setOpenSignUp] = useState(false);
-
-  const handleOpenSignIn = () => {
-    setOpenSignIn(true);
-    setOpenSignUp(false);
-  };
-
-  const handleOpenSignUp = () => {
-    setOpenSignUp(true);
-    setOpenSignIn(false);
-  };
-
-  const handleCloseModals = () => {
-    setOpenSignIn(false);
-    setOpenSignUp(false);
-  };
-
   return (
     <>
       <HideOnScroll {...props}>
         <AppBar position="sticky" color="default" elevation={4}>
           <TopBar />
-          <Navbar 
-            onLoginClick={handleOpenSignIn}
-            onSignUpClick={handleOpenSignUp}
-          />
+          <Navbar />
         </AppBar>
       </HideOnScroll>
-
-      <SignInModal
-        open={openSignIn}
-        onClose={handleCloseModals}
-        onSwitchToSignUp={handleOpenSignUp}
-      />
-      <SignUpModal
-        open={openSignUp}
-        onClose={handleCloseModals}
-        onSwitchToSignIn={handleOpenSignIn}
-      />
     </>
   );
 }
